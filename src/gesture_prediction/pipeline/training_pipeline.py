b5cd0ed022d8720a9075435c3ed13fd868d22f84
Prakhyath Bhandary <prakhz16@gmail.com>
2022-12-13 11:32:04 +0530

    data validation stage added

--- src/gesture_prediction/pipeline/training_pipeline.py
+++ src/gesture_prediction/pipeline/training_pipeline.py
@@ -1,9 +1,10 @@
-from gesture_prediction.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
-from gesture_prediction.entity.artifact_entity import DataIngestionArtifact
+from gesture_prediction.entity.config_entity import (TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig)
+from gesture_prediction.entity.artifact_entity import (DataIngestionArtifact,DataValidationArtifact)
 from gesture_prediction.exception import GestureException
 import sys,os
 from gesture_prediction.logger import logging
 from gesture_prediction.components.data_ingestion import DataIngestion
+from gesture_prediction.components.data_validation import DataValidation
 from gesture_prediction.constants.training_pipeline import SAVED_MODEL_DIR
 
 
@@ -25,6 +26,19 @@ class TrainPipeline:
             return data_ingestion_artifact
         except  Exception as e:
             raise  GestureException(e,sys)
+    
+    def start_data_validaton(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
+        try:
+            logging.info("Starting data validation")
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+            data_validation_config = data_validation_config
+            )
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info(f"Data validation completed and artifact: {data_validation_artifact}")
+            return data_validation_artifact
+        except  Exception as e:
+            raise  GestureException(e,sys)
 
 
     def run_pipeline(self):
@@ -32,6 +46,7 @@ class TrainPipeline:
             TrainPipeline.is_pipeline_running = True
             logging.info("Starting training pipeline")
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
             logging.info("Training pipeline completed")
             
         except  Exception as e:
